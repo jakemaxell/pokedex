b7f8595e1e53fac8076251e6215eab21c064b066
Jacob Maxwell <37555603+jakemaxell@users.noreply.github.com>
2024-06-05 17:07:11 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -12,11 +12,21 @@ document.getElementById('search-bar').addEventListener('keydown', function(event
 function search(){
     const pokemon = document.getElementById('search-bar').value;
     fetch('https://pokeapi.co/api/v2/pokemon/' + pokemon).then(response => {
+        if(!response.ok){
+            alert("There was an issue with your request. . .");
+            return;
+        }
         return response.json();
     }).then(data => {
         const pokemonNumber = data['forms']['0']['url'];
-        const formattedPokemonNumber = pokemonNumber.slice(-4, -1);
+        const formattedPokemonNumber = pokemonNumber.match(/\b\d+\b/)[0];
         alert('Name: ' + data['name'] + '\nPokedex Index Number: ' + formattedPokemonNumber);
+
+        var image = document.getElementById('pokemon-image');
+        image.src = data['sprites']['front_default'];
+
         console.log(data);
     });
+
+    document.getElementById('search-bar').value = '';
 };
